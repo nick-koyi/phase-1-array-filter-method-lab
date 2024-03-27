45e90bc0bcebe3aaddbc34ee28cab6d9a9858dd3
nicholas koyi <koyinicholas@gmail.com>
2024-03-27 14:55:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(arr, name) {
+  return arr.filter((item) =>{
+    return item.toLowerCase() === name.toLowerCase()
+});
+}
+
+function fuzzyMatch(arr,str) {
+  return arr.filter(item =>
+     item.toLowerCase().startsWith(str.toLowerCase())
+  );
+}
+
+function matchName(arr, str) {
+  return arr.filter(item =>
+    item.name.toLowerCase() === str.toLowerCase()
+  );
+}
+
+/*
+const arr = [
+  { name: 'Alice', hometown: 'Seattle' },
+  { name: 'Bob', hometown: 'New York' },
+  { name: 'Charlie', hometown: 'San Francisco' },
+  { name: 'alice', hometown: 'Los Angeles' }
+];
+*/
